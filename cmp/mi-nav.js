7c4592e7f98fe79a1951125181120e20cd6ad212
PizzasCastilloOficial <PizzasCastilloOficial@gmail.com>
2021-12-15 12:33:05 -0600

    nav

--- cmp/mi-nav.js
+++ cmp/mi-nav.js
@@ -1,62 +1,62 @@
-import {
-    cargaRoles
-} from "../js/seguridad.js";
-import {
-    getAuth
-} from "../lib/fabrica.js";
-import {
-    muestraError
-} from "../lib/util.js";
+// @ts-nocheck
+import { cargaRoles } from "../js/seguridad.js";
+import { getAuth } from "../lib/fabrica.js";
+import { muestraError } from "../lib/util.js";
 
 class MiNav extends HTMLElement {
     connectedCallback() {
         this.innerHTML = /* html */
-            `<ul>
-                <li>
-                <a href="index.html">Inicio</a>
-                <a href="sesion.html">Sesión</a>
-                </li>
-            </ul>`;
-        this.ul =
-            this.querySelector("ul");
-        getAuth().onAuthStateChanged(
-            usuario => this.cambiaUsuario(usuario),
-            muestraError);
+            `<header>
+          <div class="menu">
+            <a href="index.html">
+              <img src="img/logo.png" alt="">
+            </a>
+            <nav>
+                <ul>
+                  <li><a href="index.html">Inicio</a></li>
+                  <li><a href="sesion.html">Sesión</a></li>
+                </ul>
+            </nav>
+          </div>
+        </header>`;
+        this.ul = this.querySelector("ul");
+        getAuth().onAuthStateChanged(usuario =>
+            this.cambiaUsuario(usuario), muestraError);
     }
 
-    /**
-     * @param {import(
-        "../lib/tiposFire.js").User}
-        usu */
+    /** @param {import("../lib/tiposFire.js").User} usu */
     async cambiaUsuario(usu) {
         if (usu && usu.email) {
             let html = "";
-            const roles =
-                await cargaRoles(
-                    usu.email);
+            const roles = await cargaRoles(usu.email);
+
+            /* Enlaces para clientes */
             if (roles.has("Cliente")) {
                 html += /* html */
                     `<li>
                         <a href="comentarios.html">Comentarios</a>
                     </li>`;
             }
+
+            /* Enlaces para empleados */
             if (roles.has("Empleado")) {
                 html += /* html */
                     `<li>
-                        <a href= "platillos.html">Platillos</a>
-                        <a href= "sucursales.html">Sucursales</a>
+                        <a href="platillos.html">Platillos</a>
+                        <a href="sucursales.html">Sucursales</a>
                     </li>`;
             }
+
+            /* Enlace para administrador */
             if (roles.has("Administrador")) {
                 html += /* html */
                     `<li>
-                    <a href= "empleados.html">Empleados</a>
-                </li>`;
+                        <a href="empleados.html">Empleados</a>
+                    </li>`;
             }
             this.ul.innerHTML += html;
         }
     }
 }
 
-customElements.define(
-    "mi-nav", MiNav);
+customElements.define("mi-nav", MiNav);
